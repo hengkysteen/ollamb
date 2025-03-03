989f6a0f4a50b2f4e3f71897ec7fb019f7a528a8
Hengky Steen <hengkysteen@gmail.com>
2025-03-04 01:58:10 +0700

    add notif on small devices

--- app/lib/src/features/splash/splash_vm.dart
+++ app/lib/src/features/splash/splash_vm.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:ollamb/src/core/core.dart';
 import 'package:ollamb/src/ui/pages/main_page.dart';
 import 'package:wee_kit/wee_kit.dart';
 
@@ -8,9 +9,15 @@ class SplashVm extends GetxController {
   SplashVm(this.context);
 
   Future<void> init() async {
-    await Future.delayed(const Duration(milliseconds: 600));
+    await Future.delayed(const Duration(milliseconds: 500));
+    Widget target;
     if (!context.mounted) return;
-    return WeeGo.to(context, target: const MainPage(), style: WeeGoStyle.fade, replace: true);
+    if (Core.platform.isMobileSize(context)) {
+      target = const Material(child: Center(child: Text("This app is not yet supported on small devices.")));
+    } else {
+      target = const MainPage();
+    }
+    return WeeGo.to(context, target: target, style: WeeGoStyle.fade, replace: true);
   }
 
   @override
--- app/lib/src/services/platfrom.dart
+++ app/lib/src/services/platfrom.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart' show BuildContext, MediaQuery;
 
 class PlatfromService {
   bool get isMobile {
@@ -18,4 +19,8 @@ class PlatfromService {
   }
 
   bool get isWeb => kIsWeb;
+
+  bool isMobileSize(BuildContext context) {
+    return isWeb && MediaQuery.of(context).size.width < 500;
+  }
 }
